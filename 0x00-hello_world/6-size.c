4d8b5ef5acaf65fb61a97e274a3afc7bcb271278
Mark Thuita <markthuita@gmail.com>
2022-06-06 09:26:26 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 /*sizeof evaluates the size of a variable*/
-printf("Size of char: %d byte\n", sizeof(char));
-printf("Size of int: %d bytes\n", sizeof(int));
-printf("Size of long int: %d bytes\n", sizeof(long int));
-printf("Size of long long int: %d bytes\n", sizeof(long long int));
-printf("Size of float: %d byte\n", sizeof(float));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
