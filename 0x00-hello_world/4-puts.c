a6c50121bf0db59a73b9b22b553dd5343b4675ed
Mark Thuita <markthuita@gmail.com>
2022-06-06 07:45:33 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * function prints Programming is like building a multilingual puzzle
+ * followed by a new line.
+ * returns always 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * function prints with proper grammar, but the outcome is a piece of art
+ * followed by a new line
+ * return always 0 (success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
